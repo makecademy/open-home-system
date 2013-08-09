a7c1e6660e72db3ac08ade8501150a12c28427b9
Marco <marco@Marc-Oliviers-MacBook-Pro.local>
2013-08-09 11:34:42 -0400

    first version temp case

--- hardware/motion/sketches/motion_detector_test/motion_detector_test.ino
+++ hardware/motion/sketches/motion_detector_test/motion_detector_test.ino
@@ -1,4 +1,5 @@
 const int sensor_pin = 8;
+int audio_pin = 7;
 
 void setup() {
   Serial.begin(9600);
@@ -9,7 +10,12 @@ void loop() {
   // Get sensor value
   int sensor_value = digitalRead(sensor_pin);
   
+  // Make a sound if something is detected
+    if (sensor_value){
+        tone(audio_pin, 440, 10);
+    }
+  
   // Print on serial
   Serial.println(sensor_value);
-  delay(1000);
+  delay(10);
 }
--- hardware/temperature/case/temperature_front.scad
+++ hardware/temperature/case/temperature_front.scad
@@ -26,11 +26,6 @@ difference(){
 		}
 	}
 
-	// Sensor hole
-	translate([1,w/2-sensor_w/2,l-thickness-distance_from_top-sensor_l]) {
-			cube([50,sensor_w,sensor_l]);
-		} 
-
 	// Clips on the sides
 	translate([distance_from_edge,-5,l/2]) {
 		cube([attach_clip_l,60,attach_clip_w]);
