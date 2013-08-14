6e0e00505dbc7e4fb7cf5ab34f71dbb4fad8a5c7
Marco <marco@Marc-Oliviers-MacBook-Pro.local>
2013-08-14 10:16:23 -0400

    basic sketches motion and temp

--- hardware/temperature/sketches/temperature_sensor_test/temperature_sensor_test.ino
+++ hardware/temperature/sketches/temperature_sensor_test/temperature_sensor_test.ino
@@ -1,20 +1,19 @@
 /*************************************************** 
-  This is a sketch to test the motion detection using the Parallax PIR sensor.
+  This is a sketch to test the temperature measurement using the TMP36 sensor.
   
   Written by Marco Schwartz for Open Home System.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
  
-const int sensor_pin = 8; // Sensor pin
+const int sensor_pin = 0; // Sensor on analog 0 pin
 
 void setup() {
   Serial.begin(9600);
-  delay(1000);
 }
 
 void loop() {
   // Get sensor value
-  int sensor_value = digitalRead(sensor_pin);
+  int sensor_value = analogRead(sensor_pin);
   
   // Print on serial
   Serial.println(sensor_value);
